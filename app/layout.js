a19c22947f3d7e83bcda88387d16a64538580c60
ayelenvillega42-dotcom <ayelenvillega42@gmail.com>
2026-08-28 19:21:52 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Portal Integral del Asesor",
+  description: "Portal Integral del Asesor",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
